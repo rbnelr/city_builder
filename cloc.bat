046fa59c9a28507e486fc37b59113bd0584e0764
rbnelr <rbenr@gmail.com>
2023-10-27 00:34:10 +0200

    cloc

---
+++ cloc.bat
@@ -0,0 +1,22 @@
+
+@echo.
+@echo.
+@echo ======================== just project specific code ======================
+C:\apps\cloc.exe  src shaders --exclude-dir=engine --by-file
+
+@echo.
+@echo.
+@echo ======================== framework code ==================================
+C:\apps\cloc.exe src/engine --exclude-dir=dear_imgui,glad,kisslib,tracy --by-file
+
+@echo.
+@echo.
+@echo ======================== kisslib =========================================
+C:\apps\cloc.exe src/engine/kisslib --exclude-dir=nlohmann_json,smhasher,stb,output --by-file 
+
+@echo.
+@echo.
+@echo ======================== kissmath generated files ========================
+C:\apps\cloc.exe src/engine/kisslib/kissmath/output --by-file
+
+pause
